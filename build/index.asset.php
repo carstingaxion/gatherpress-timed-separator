406697f91018dc076c9fbc1680374748b4d014f4
Carsten Bach <mail@carsten-bach.de>
2026-04-30 03:11:30 +0200

    build

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '37d3d96c96145d2af433');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '93141bf07cf227973de9');
--- build/render.php
+++ build/render.php
@@ -9,16 +9,20 @@
  * Available variables (provided by WordPress block rendering context):
  *
  * @var array<string, mixed> $attributes Block attributes.
- * @var string $content Inner block content (empty for this block).
- * @var WP_Block $block The block instance, including context.
+ * @var string               $content    Inner block content (empty for this block).
+ * @var WP_Block             $block      The block instance, including context.
  *
  * @package GatherpressTimedSeparator
  * @since   0.1.0
  */
 
+declare(strict_types=1);
+
 if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-$renderer = GatherpressTimedSeparator\Renderer::get_instance();
-echo $renderer->render( $attributes, $content, $block );
+$gatherpress_timed_separator_renderer = GatherpressTimedSeparator\Renderer::get_instance();
+
+// phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped -- Renderer::render() escapes all output internally.
+echo $gatherpress_timed_separator_renderer->render( $attributes, $content, $block );
